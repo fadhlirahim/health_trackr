3309bbb61d7b5b13878f1b2c9e3714e779ad8524
Fadhli Rahim <fadhlirahim@gmail.com>
2022-09-12 11:58:56 +0800

    Add tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :health_trackr, HealthTrackrWeb.Endpoint,
   secret_key_base: "5gLLTGg2ero3mTmaflNS9mZlFRFpcQLQpcqCRYV7EeC8fgvKc5yxZys8S4ekjfWm",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
