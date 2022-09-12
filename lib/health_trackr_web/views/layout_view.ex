843b63058ee62b1cae5b404aa7f5b0d9f24961bc
Fadhli Rahim <fadhlirahim@gmail.com>
2022-09-12 16:11:46 +0800

    Layout with large background image. Tailwind example

--- lib/health_trackr_web/views/layout_view.ex
+++ lib/health_trackr_web/views/layout_view.ex
@@ -1,5 +1,6 @@
 defmodule HealthTrackrWeb.LayoutView do
   use HealthTrackrWeb, :view
+  use PetalComponents
 
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
