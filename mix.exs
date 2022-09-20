0d161d6f537d1ece1751abe6d7f84cfa7db34979
Fadhli Rahim <fadhlirahim@gmail.com>
2022-09-20 16:55:34 +0800

    Add faker

--- mix.exs
+++ mix.exs
@@ -49,6 +49,7 @@ defmodule HealthTrackr.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
+      {:faker, "~> 0.17.0"},
       {:petal_components, "~> 0.17"},
       {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
     ]
